e0852c640ef34a755c636532a26375f277c503ee
t.bjargrim <therese.bjargrim@student.kyh.se>
2020-12-08 10:55:46 +0100

    working templetes

--- quill.js
+++ quill.js
@@ -1,52 +1,3 @@
-// let templeteButtonOne = document.querySelector('#t1');
-// let templeteButtonTwo = document.querySelector('#t2');
-// let templeteButtonThree = document.querySelector('#t3');
-
-// let Inline = Quill.import('blots/inline');
-
-// class SpanBlock extends Inline{    
-
-//     static create(value){
-//         let node = super.create();
-//         node.setAttribute('class','spanblock');
-//         return node;    
-//     } 
-// }
-
-// SpanBlock.blotName = 'spanblock';
-// SpanBlock.tagName = 'div';
-// Quill.register(SpanBlock);
-
-// templeteButtonOne.addEventListener('click', function() {
-
-
-//   //apply it to the text
-//     let range = quill.getSelection();
-//         if(range){
-//             quill.formatText(range,'spanblock',true);
-//         }else{
-
-//         }
-//   console.log('Templete One: Clicked!');
-// });
-
-
-
-// templeteButtonTwo.addEventListener('click', function() {
-
-
-//   console.log('Templete Two : Clicked!');
-// });
-// templeteButtonThree.addEventListener('click', function() {
-//   console.log('Templete Three : Clicked!');
-// });
-
-
-//     let add = document.querySelector('#t1');
-//   //Only add it ones!!! Needs to know that it has been clicked, if...else...
-//     add.className += "addStyle";
-
-
 let quill = new Quill ('#editor',  {
     modules:{
         toolbar:'#toolbar-container'
@@ -56,40 +7,25 @@ let quill = new Quill ('#editor',  {
 
 //how to take the othe classes away when pressing normal???
 
-var customButtonNormal = document.querySelector('#customNormal');
-var customButton = document.querySelector('#customOne');
-var customButtonTwo = document.querySelector('#customTwo');
-var customButtonThree = document.querySelector('#customThree');
+let customButton = document.querySelector('#customOne');
+let customButtonTwo = document.querySelector('#customTwo');
+let customButtonThree = document.querySelector('#customThree');
 
 let content = document.querySelector('.ql-editor')
 
+
 customButton.addEventListener('click', function() {
-console.log('clicked')
-  if(quill.getFormat()) {
-    content.classList.add('ql-active');
-  } else {
-    content.classList.remove('ql-active');
-  }
-});
-//the class removes when the next one is being pushed ???
-
-customButtonTwo.addEventListener('click', function() {
-console.log('clicked')
-  if(quill.getFormat()) {
-    content.classList.add('customTwo');
-  } else {
-    content.classList.remove('customTwo');
-  }
+content.classList.toggle('customOne') 
+// return;
 });
 
-customButtonThree.addEventListener('click', function() {
-console.log('clicked')
-  if(quill.getFormat()) {
-    content.classList.add('customThree');
-  } else {
-    content.classList.remove('customThree');
-  }
+customButtonTwo.addEventListener('click', function(){
+  content.classList.toggle('customTwo')
 });
 
-//Date function
-//Year, month, day, time
+customButtonThree.addEventListener('click', function(){
+  content.classList.toggle('customThree')
+});
+
+
+
